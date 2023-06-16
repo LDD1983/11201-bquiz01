ebe9073a7e81a21c928af3a139d5f2a13d674df9
mackliu <macklun@gmail.com>
2023-06-16 11:47:27 +0800

    整合更新的api為一支update.php

--- back/ad.php
+++ back/ad.php
@@ -1,6 +1,6 @@
 <div style="width:99%; height:87%; margin:auto; overflow:auto; border:#666 1px solid;">
     <p class="t cent botli"><?=$Ad->header;?></p>
-    <form method="post" action="./api/update_ad.php">
+    <form method="post" action="./api/update.php">
         <table width="100%">
             <tbody>
                 <tr class="yel">
@@ -36,6 +36,7 @@
         <table style="margin-top:40px; width:70%;">
             <tbody>
                 <tr>
+                <input type="hidden" name="table" value='ad'>
                     <td width="200px"><input type="button" onclick="op('#cover','#cvr','./modal/ad.php')" value="新增動態文字廣告"></td>
                     <td class="cent"><input type="submit" value="修改確定"><input type="reset" value="重置"></td>
                 </tr>
