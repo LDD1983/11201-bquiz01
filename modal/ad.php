909c252fee9e78627949a0434c081592914290ba
mackliu <macklun@gmail.com>
2023-06-16 14:32:25 +0800

    整合add的功能為一支獨立的add.php,可以支應不同功能的新增資料

--- modal/ad.php
+++ modal/ad.php
@@ -1,12 +1,13 @@
 <h3>新增動態文字廣告</h3>
 <hr>
-<form action="./api/add_ad.php" method="post" enctype="multipart/form-data">
+<form action="./api/add.php" method="post" enctype="multipart/form-data">
     <table>
         <tr>
             <td>動態文字廣告：</td>
             <td><input type="text" name='text'></td>
         </tr>
         <tr>
+            <input type="hidden" name="table" value='ad'>
             <td><input type="submit" value="新增"></td>
             <td><input type="reset" value="重置"></td>
         </tr>
--- modal/mvim.php
+++ modal/mvim.php
@@ -1,12 +1,13 @@
 <h3>新增動畫圖片</h3>
 <hr>
-<form action="./api/add_mvim.php" method="post" enctype="multipart/form-data">
+<form action="./api/add.php" method="post" enctype="multipart/form-data">
     <table>
         <tr>
             <td>動畫圖片：</td>
             <td><input type="file" name='img'></td>
         </tr>
         <tr>
+        <input type="hidden" name="table" value='mvim'>
             <td><input type="submit" value="新增"></td>
             <td><input type="reset" value="重置"></td>
         </tr>
