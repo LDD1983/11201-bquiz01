b54229b728eea4c22fd2c1590be56db761dbed41
mackliu <macklun@gmail.com>
2023-06-19 11:27:34 +0800

    將彈出視窗的html碼獨立出來,使用物件的方式來呈現,並整合多個彈出視窗為1支程式

--- back/image.php
+++ back/image.php
@@ -39,7 +39,7 @@
             <tbody>
                 <tr>
                     <input type="hidden" name="table" value="image">
-                    <td width="200px"><input type="button" onclick="op('#cover','#cvr','./modal/image.php')" value="新增網站標題圖片"></td>
+                    <td width="200px"><input type="button" onclick="op('#cover','#cvr','./modal/add_form.php?table=image')" value="新增網站標題圖片"></td>
                     <td class="cent"><input type="submit" value="修改確定"><input type="reset" value="重置"></td>
                 </tr>
             </tbody>
