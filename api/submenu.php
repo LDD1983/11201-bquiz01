99975923029c00f8af123670cf89cd3ad53ed871
mackliu <macklun@gmail.com>
2023-06-26 14:33:30 +0800

    完成新增次選單及顯示次選單功能

---
+++ api/submenu.php
@@ -0,0 +1,30 @@
+<?php 
+include_once "../base.php";
+
+$main_id=$_POST['main_id'];
+
+
+
+if(isset($_POST['text2'])){
+    foreach($_POST['text2'] as $idx => $text){
+        if($text!=""){
+            $Menu->save([
+                'text'=>$text,
+                'href'=>$_POST['href2'][$idx],
+                'sh'=>1,
+                'main_id'=>$main_id
+            ]
+            );
+        }
+    }
+}
+
+to("../backend.php?do=menu");
+
+
+
+
+?>
+
+
+
