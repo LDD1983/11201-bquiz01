87e310d5a2deb5610af54bcb164bc0084814cb98
mackliu <macklun@gmail.com>
2023-06-28 10:00:50 +0800

    套用內建的js程式及修改css,讓主選單及次選單可以顯示出來

--- index.php
+++ index.php
@@ -39,6 +39,10 @@
 						echo 	"<a href='{$row['href']}'>";
 						echo 		$row['text'];
 						echo 	"</a>";
+						echo "<div class='mw'>";
+						echo 	"<div class='mainmu2'>次選單</div>";
+						echo 	"<div class='mainmu2'>次選單</div>";
+						echo "</div>";
 						echo "</div>";
 
 					}
